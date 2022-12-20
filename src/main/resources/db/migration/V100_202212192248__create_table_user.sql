7c32c482e6a5613a99040da62146d7d87d7efafc
Joaonorr <joaonoroes.jn@gmail.com>
2022-12-19 23:29:29 -0300

    feat: create hibernate_sequence and users migration script

---
+++ src/main/resources/db/migration/V100_202212192248__create_table_user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL
+);
