351651883f2f89c8ed59e45417de3ad7269709bc
Joaonorr <joaonoroes.jn@gmail.com>
2022-12-19 23:25:08 -0300

    Init service project

---
+++ src/test/java/com/demo/meetup/MeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
